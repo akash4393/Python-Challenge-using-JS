c72a3f1a64c9c6686a42e994f1c18802cc3513da
Akash Singh <akash4393@gmail.com>
2017-12-07 22:12:23 -0800

    Challenge 1 complete

--- Challenge 1/1.js
+++ Challenge 1/1.js
@@ -4,9 +4,23 @@ jQuery(document).ready(function() {
 		"<p>Input String:</p><br><p>g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.</p><br><p>Solution String:</p><br><p>"
 		);
 	var inputStr="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-	inputStr.split("").forEach((char) => {
-		if(char != " ")
+	var strTransform = function(char) {
+		if((char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 88) || (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 120))
 		char=String.fromCharCode(char.charCodeAt(0) + 2);
+		else if(char.charCodeAt(0) === 89)
+		char=String.fromCharCode(65);
+		else if(char.charCodeAt(0) === 90)
+		char=String.fromCharCode(66);
+		else if(char.charCodeAt(0) === 121)
+		char=String.fromCharCode(97);
+		else if(char.charCodeAt(0) === 122)
+		char=String.fromCharCode(98);
 		jQuery("#sol-app").append(char);
-	});
+	}
+	inputStr.split("").forEach(strTransform);
+	jQuery("#sol-app").append(
+		"</p><br><p>URL string: map</p><br><p>URL String Transformed:</p><br><p>"
+		);
+	"map".split("").forEach(strTransform);
+	jQuery("#sol-app").append("</p>")
 });
