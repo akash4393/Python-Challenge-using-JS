5675eb0f38e6d52015a9a6bf118052bc1805185b
Akash Singh <akash4393@gmail.com>
2017-12-07 23:18:59 -0800

    Challenge 2 progress 1

---
+++ Challenge 2/2_node.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+var outputString = "Special hidden characters: \n";
+var inputString = fs.readFileSync(__dirname + '/input_file.txt', 'utf-8');
+//console.log(inputString);
+var inputStringArr = inputString.split("");
+//console.log(inputStringArr);
+var lookForChar = function(char){
+	if(char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90 || char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122)
+	outputString+=char
+}
+
+inputStringArr.forEach(lookForChar);
+console.log(outputString);
