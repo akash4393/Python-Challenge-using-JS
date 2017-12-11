172b6bd33df46b0dd35401cefac569e98f68a400
Akash Singh <akash4393@gmail.com>
2017-12-11 02:09:26 -0800

    Challenge 4 complete

--- Challenge 4/4_node.js
+++ Challenge 4/4_node.js
@@ -1,10 +1,38 @@
 const http = require('http');
 
-var url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345';
+var count = 0;
+var urlBase = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=';
+//var nextQuery = '12345'; //initial condition
+var nextQuery = '8022'; //new program start point after 'Yes. Divide by two and keep going.' response
+var url = urlBase + nextQuery;
+var resBody;
 
-http.get(url, res => {
+var resFunction = function (res) {
 	res.setEncoding("utf8")
+	if(count >= 399){
+		res.on('data', data => {
+			console.log(data);
+			return 1;
+		})
+	}
 	res.on('data', data => {
-		console.log(data);
+		var exp = new RegExp("[nothing]{7}\\s[is]{2}\\s[0-9]{1,5}", 'g');
+		var nothingIsString = exp.exec(data);
+		if(!nothingIsString){
+			console.log(data);
+			return 1;
+		}
+		var expNumber = new RegExp("[0-9]{1,5}", 'g');
+		var resBodyNumber = expNumber.exec(nothingIsString[0]);
+		if(!resBodyNumber){
+			console.log(data);
+			return 1;
+		}
+		var resBody = resBodyNumber[0];
+		nextQuery = resBody;
+		url = urlBase + nextQuery;
+		count+=1;
+		return http.get(url, resFunction)
 	});
-});
+}
+http.get(url, resFunction);
